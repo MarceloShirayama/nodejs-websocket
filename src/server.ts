c1ac4d31e68fa72526d407c782248744d312b80c
MarceloShirayama <caixadecorreiodomarcelo@gmail>
2021-05-06 13:05:40 -0300

    create server and fisrt example route

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { request, response } from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Olá NLW 5!" })
+});
+
+app.listen(3333, () => {
+  console.log("Server on http://localhost:3333")
+});
