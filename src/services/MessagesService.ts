4c9bc693f8368e99eb37047123ac54662c013f87
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-05-08 14:18:02 -0300

    add listByUser function in messages service

--- src/services/MessagesService.ts
+++ src/services/MessagesService.ts
@@ -17,4 +17,12 @@ export class MessagesService {
 
     return message;
   }
+
+  async listByUser(user_id: string) {
+    const messagesRepository = getCustomRepository(MessagesRepository);
+
+    const list = await messagesRepository.find({ user_id });
+
+    return list;
+  }
 }
