f3f1035a43206b2d1a957916f9ec3c0212996e28
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-05-08 14:49:01 -0300

    refactor messages, settings and users services

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,20 +1,24 @@
-import { getCustomRepository } from "typeorm"
-import { UsersRepository } from "../repositories/UsersRepository"
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository";
 
 
 export class UsersService {
-  async create(email: string) {
-    const usersRepository = getCustomRepository(UsersRepository);
+  private usersRepository: Repository<User>
 
-    const userExist = await usersRepository.findOne({ email });
+  constructor() {
+    this.usersRepository = getCustomRepository(UsersRepository);
+  }
+  async create(email: string) {
+    const userExist = await this.usersRepository.findOne({ email });
 
     if (userExist) {
       return userExist;
     }
 
-    const user = usersRepository.create({ email });
+    const user = this.usersRepository.create({ email });
 
-    await usersRepository.save(user);
+    await this.usersRepository.save(user);
 
     return user;
   }
