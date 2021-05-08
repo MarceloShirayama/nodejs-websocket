1bfc5edf27b8551148b231c9f5ae6362a25427ce
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-05-08 13:59:13 -0300

    create messages controller

---
+++ src/controller/MessagesController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { MessagesService } from '../services/MessagesService';
+
+export class MessagesController {
+  async create(request: Request, response: Response) {
+    const { admin_id, text, user_id } = request.body;
+
+    const messagesServices = new MessagesService();
+
+    const message = await messagesServices.create({ admin_id, text, user_id });
+
+    return message;
+  }
+}
