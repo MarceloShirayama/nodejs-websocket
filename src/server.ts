1206a59d295cdadb779c80e1025bfcf05cd31050
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-05-09 10:15:55 -0300

    configure public folder html on routes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import express, { request, response } from "express";
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
+import path from "path";
+import ejs from "ejs";
 import './database';
 import { routes } from "./routes";
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.set("views", path.join(__dirname, "..", "public"));
+app.engine("html", ejs.renderFile);
+app.set("view engine", "html");
 
 const http = createServer(app); // protocolo http
 const io = new Server(http); // protocolo websocket
