3e1168aafb13fa12a6610d2733f8f57169d8840f
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-05-09 10:06:07 -0300

    create http and socket.io protocol

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,23 @@
 import express, { request, response } from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
 import './database';
 import { routes } from "./routes";
 
 const app = express();
 
+const http = createServer(app); // protocolo http
+const io = new Server(http); // protocolo websocket
+
+io.on("connection", (socket: Socket) => {
+  console.log(`Web socket conectado: ${socket.id}`);
+  
+});
+
 app.use(express.json());
 
 app.use(routes)
 
-app.listen(3333, () => {
+http.listen(3333, () => {
   console.log("Server on http://localhost:3333")
 });
